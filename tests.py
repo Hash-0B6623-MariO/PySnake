a5ee6c2b4bbf231f9fb7bf8b157403dbeb0ec06b
darlenelovitos <your-github-email@example.com>
2025-12-26 02:59:14 +0800

    1

--- tests.py
+++ tests.py
@@ -50,8 +50,6 @@ class Tile:
 
     # Helper function for drawing
     def scale_to_board(self, tile_unit):
-        print(self.position)
-        print(self.personality)
         return tuple(pos * tile_unit for pos in self.position)
 
     def draw(self, surface:pygame.Surface, scale=1):
@@ -118,23 +116,12 @@ class Entity:
         def change_direction(self, direction):
             self.direction = direction
             
-        
-        # Two solutions 
-        #   (Bad=recursive call till the last tail is found to be given the back obj)
-        #   (Long=replaces the current head to now be a tail, have to deal with id as another thing to store)
-        
-        # Temporary fix
-        def get_tail(self):
-            return self if self.back == None else self.back.get_tail()
+        def grow(self, new_pos):
+            head_new = Entity.Char(position=new_pos, Head=True)
+            self.personality = 1
+            head_new.back = self
+            return head_new 
 
-        # TODO: Please implementing anything else
-        def grow(self, tail_old):
-            tail_curr = self.get_tail()
-            tail_new = Entity.Char(tail_old.position, tail_old.direction)
-
-            tail_curr.back = tail_new
-            tail_new.front = tail_curr
-        
         # TODO: NEW_POS here to deal with the calls from boardrules
         def move(self, new_position=None):
             new_position = self.get_front()
@@ -146,6 +133,10 @@ class Entity:
             if self.back:
                 self.back.draw(surface, scale)
             super().draw(surface, scale)
+    
+        # Utility functions
+        def get_tail(self):
+            return self if self.back == None else self.back.get_tail()
         
 
 class GameBoard(pygame.Surface):
@@ -154,7 +145,7 @@ class GameBoard(pygame.Surface):
         self.tile_unit = tile_unit
         self.bounds = dimensions
         self.map = self.create_map()
-        self.instances = {}    # Tracks all instances tiles on the board. Struct is {position: tile id}
+        self.instances = []    # Tracks all instances tiles on the board.
 
         # Store tile palettes here
         self.__color =((0,0,0), (255,255,255)) # TODO: Change to a dictionary
@@ -170,13 +161,12 @@ class GameBoard(pygame.Surface):
         x, y = self.bounds
         return [[0 for _ in range(x)] for _ in range(y)]
     
-    # TODO: Memory intensive...
     def get_available(self):
         x, y = self.bounds
         return list(set([(y2,x2) for y2 in range(0,x) for x2 in range(0,y)]) - set(self.get_occupied()))
 
     def get_occupied(self):
-        return tuple(pos for pos, tile in self.instances)
+        return tuple(tile.position for tile in self.instances)
     
     def get_center(self):
         x, y = self.bounds
@@ -191,11 +181,8 @@ class GameBoard(pygame.Surface):
 
     # Could be better optimized
     def draw_tiles(self):
-        for row_index, row  in enumerate(self.map):
-            for col_index, column in enumerate(row):
-                if self.map[row_index][col_index] != 0:
-                    tile = self.map[row_index][col_index]
-                    tile.draw(self, self.tile_unit)
+        for tile in self.instances:
+            tile.draw(self, self.tile_unit)
 
     def draw_board(self, window, position:tuple):
         window.fill((0, 0, 0))
@@ -206,13 +193,14 @@ class GameBoard(pygame.Surface):
     # Board Data Functions ==============================================================================================================
     def add_tile(self, tile:Tile):
         x, y = tile.position
+        self.instances.append(tile)
         self.map[x][y] = tile
-        self.instances[tile.position] = tile.personality
 
     def clear_tile(self, position):
         x, y = position
+        tile = self.get_tile(position)
+        self.instances.remove(tile)
         self.map[x][y] = 0
-        del(self.instances[position])
 
     # Searches for a tile with their position
     def get_tile(self, position:tuple):
@@ -221,15 +209,10 @@ class GameBoard(pygame.Surface):
         
     # Searches for all tiles of a certain type on the board, returns position
     def search_board(self, personality):
-        return [key for key, value in self.instances.items() if int(value) == personality]
+        return [tile for tile in self.instances if tile == personality]
 
-    # Try using this as an intermediary for clearing and adding
     def update_tile(self, position, tile=None):
-        # x, y = position
-        # self.map[x][y] = tile if tile else 0
-        # del(self.instances[position])
-        pass
-        
+        pass  
 
     # Zero utility at the moment, made to give attributes to all tiles of a type
     def update_tiles(self, personality, args):
@@ -244,11 +227,12 @@ class GameBoard(pygame.Surface):
             for key, value in args.items():
                 setattr(tile, key, value)
 
-    # The main bulk of the game logic runs here ==============================================================================================================
-    def run_game(self):
-        print()
-        pass
-
+    # Refreshes the map when positions are changed on the objects themselves
+    def refresh_map(self):
+        for tile in self.instances:
+            self.clear_tile(tile.position)
+            self.add_tile(tile)
+        
 # Game Logic built here ==============================================================================================================
 # Includes rules, win conditions, collisions and controls
 # Notes: 
@@ -269,7 +253,7 @@ class BoardRules():
 
         # Here for referencing any effects of specific tile types
         self.on_collide = {
-            0:self.character.move,
+            0:self.move_character,
             1:'asdasd',
             2:self.collide_fruit
         }
@@ -280,38 +264,35 @@ class BoardRules():
                 exit()
             if event.type == pygame.KEYDOWN:
                 if event.key in self.key_mapping:
-                    self.change_direction(self.key_mapping[event.key])
+                    self.character.change_direction(self.key_mapping[event.key])
 
     # Handling tile data functions ==============================================================================================================    
     def find_character(self) -> Entity.Char:
-        pos = self.board.search_board(9)[0]
-        char = self.board.get_tile(pos)
-        return char 
+        return self.board.search_board(9)[0]
     
     def center_character(self):
         pass
 
-    # TODO: Currently here so its simpler to call, kinda ass
-    def change_direction(self, direction:tuple):
-        self.character.change_direction(direction)
+    # Logic functions ==============================================================================================================
+    def check_collisions(self):
+        front = self.board.get_tile(self.character.get_front())
+        self.on_collide[front](front)
+
+    # On Collide Functions ------------------------------------------------------------------------------------
+    def collide_fruit(self, tile:Collectibles.Fruit):
+        pos = tile.position
+        self.board.clear_tile(pos)
+        # Inevitable source of headaces, may be overlapping
+        self.character = self.character.grow(pos)
+        self.board.add_tile(self.character)
+        self.spawn_fruit()
 
     # A bit undescriptive but it just runs a tick basically
-    def move_character(self):
-        front = self.character.get_front()
-        tile = self.board.get_tile(front)
-        self.on_collide[tile](tile)
-
-    # Logic functions ==============================================================================================================
-    def check_collisions(self, character:Tile, board:GameBoard):
-        pass
-
-    # On Collide Functions
-    def collide_fruit(self, front):
-        self.board.clear_tile(front)
-
-        tail = self.character.get_tail()
+    def move_character(self, tile=None):
         self.character.move()
-        self.character.grow(tail)
+        
+
+    # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     # Utility functions
     def spawn_fruit(self):
@@ -320,7 +301,8 @@ class BoardRules():
     # Have all the checks just be here
     def run_tick(self):
         self.key_event()
-        self.move_character()
+        self.check_collisions()
+        self.board.refresh_map()
         
 
 
@@ -335,7 +317,7 @@ class SnakeGame:
 
         # Dynamic attributes, does not change during runtime though
         self.attributes = {
-            "board_dimensions": [20, 20],
+            "board_dimensions": [9, 9],
             "board_unit": 20,
             "board_palette": ((100, 100, 100), (100, 100, 100))
         }
@@ -361,8 +343,6 @@ class SnakeGame:
         self.board.set_color(a["board_palette"])
 
         self.character = Entity.Char(position=(0,0), Head=True)  # Player tile
-        self.character.grow(Entity.Char(position=(1,0)))
-        self.character.grow(Entity.Char(position=(2,0)))
         self.board.add_tile(self.character)
 
         self.r = BoardRules(self.board)
@@ -395,17 +375,7 @@ class SnakeGame:
             # holy hack batman
             # fix, too laggy have board updates automatically handle tile updates
             # board.update_tile(char.id, char)
-
-
             pygame.time.Clock().tick(4)
-
-            
             pygame.display.flip()
-
-
-# HELLO?!??!
-# board.draw_bg(colors)
-# board.draw_board(window, ((window.get_width() - board.get_width()) // 2, (window.get_height() - board.get_height()) // 2))
-# pygame.display.flip()
 
 SnakeGame().run()
